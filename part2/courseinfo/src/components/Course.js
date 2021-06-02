fb65a6c7e001e85cebd7966709ca4e9fa23381f4
Derya Antonelli <derya.antonelli@gmail.com>
2021-06-02 16:14:34 +0100

    Add

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,38 @@
+const Header = ({ course }) => {
+  return <h2>{course.name}</h2>;
+};
+
+const Total = ({ course }) => {
+  const sum = course.parts.reduce((s, p) => s + p.exercises, 0);
+  return <h3>total of {sum} exercises</h3>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map((p) => (
+        <Part key={p.id} part={p} />
+      ))}
+    </div>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  );
+};
+
+export default Course;
